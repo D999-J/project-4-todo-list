12273a05f748c0ec557b34daa5267369c27cc708
Dancan kibet <kibetd@mail.gvsu.edu>
2023-12-04 22:23:39 -0500

    "Grade me"

--- todo.py
+++ todo.py
@@ -1,1 +1,59 @@
+# To-Do List implementation
 
+tasks = []
+
+
+def add_task(task):
+    tasks.append({"task": task, "completed": False})
+    print("Task added successfully!")
+
+
+
+def view_tasks():
+    if not tasks:
+        print("No tasks in the To-Do List.")
+    else:
+        print("Tasks:")
+        for i, task in enumerate(tasks, 1):
+            status = "Completed" if task["completed"] else "Not Completed"
+            print(f"{i}. {task['task']} - {status}")
+
+
+def mark_completed(task_number):
+    try:
+        task_number = int(task_number)
+        if 1 <= task_number <= len(tasks):
+            tasks[task_number - 1]["completed"] = True
+            print("Task marked as completed.")
+        else:
+            print("Invalid task number.")
+    except ValueError:
+        print("Invalid input. Please enter a number.")
+
+
+def delete_task(task_number):
+    try:
+        task_number = int(task_number)
+        if 1 <= task_number <= len(tasks):
+            del tasks[task_number - 1]
+            print("Task deleted successfully.")
+        else:
+            print("Invalid task number.")
+    except ValueError:
+        print("Invalid input. Please enter a number.")
+
+
+# Example usage
+add_task("Learn Python")
+add_task("Build a To-Do List")
+add_task("Study amply for examination")
+add_task("Join the virtual training")
+
+# Scenarios
+view_tasks()
+
+mark_completed(1)
+view_tasks()
+
+delete_task(3)
+view_tasks()
---
+++ todo_tests.py
@@ -0,0 +1,36 @@
+import unittest
+import todo
+
+class MyTestCase(unittest.TestCase):
+
+    def test_add_task(self):
+        task = "Learn Python"
+        task = "Finish project 4 on time"
+        task = "Study amply for the final examination"
+        todo.add_task(task)
+
+    def test_view_tasks_empty_list(self):
+        view_tasks_output = todo.view_tasks()
+
+    def test_view_tasks(self):
+        todo.add_task("Learn Python")
+        todo.add_task("Join the virtual training")
+        todo.view_tasks()
+
+    def test_mark_completed_valid_task(self):
+        todo.add_task("Learn Python")
+        todo.mark_completed(1)
+        self.assertTrue(todo.tasks[0]["completed"])
+
+    def test_mark_completed_invalid_task(self):
+        result = todo.mark_completed("1")
+        self.assertEqual(result, None)
+
+    def test_delete_task_valid_task(self):
+        todo.add_task("Learn Python")
+        todo.add_task("Build a To-Do List")
+        result = todo.delete_task(1)
+
+
+if __name__ == "__main__":
+    unittest.main()
